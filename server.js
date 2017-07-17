113c5a3edf2a19ba19b38c67d9ea9b0c7cb3a491
Sumit Joshi <sumitjoshi479@gmail.com>
2017-07-17 17:06:21 +0530

    added bookModel and filter query for api search

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const Book = require('./models/bookModel');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const bookRouter = express.Router();
 bookRouter.get('/books', (req, res) => {
-    let responseJSON = { hello: "api" };
-    res.json(responseJSON);
+    
+    const query = {};
+
+    if(req.query.genre) {
+        query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.json(books);
+        }
+    });
+    // res.json(responseJSON);
+});
+
+bookRouter.get('./books/:bookId', (req,res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.json(book);
+        }
+    });
 });
 
 app.use('/api', bookRouter);
